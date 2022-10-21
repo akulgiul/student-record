54333b90ccfe9373251e43f1581e98d6c4e02744
akulgiul <luka@4luig.de>
2022-10-21 22:53:58 +0200

    first commit, started with students attributes and course types

---
+++ course.h
@@ -0,0 +1,20 @@
+enum Degree {
+	Bachelor,
+	Master
+};
+
+enum Course {
+	Art,
+	Biology,
+	Chemistry,
+	CS,
+	Engineering,
+	History,
+	Int_Relations,
+	Law,
+	Mathematics,
+	Medicine,
+	Philosophy,
+	Physics,
+	Psychology
+};
---
+++ student.h
@@ -0,0 +1,10 @@
+#include "course.h"
+
+typedef struct {
+	unsigned int student_id;
+	char *name;
+	char *surname;
+	Degree degree;
+	Course course;
+	int semester;
+} Student;
