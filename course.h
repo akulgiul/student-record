247175b0c962e3339937099c879d2e4f362bb8cd
akulgiul <luka@4luig.de>
2022-10-22 22:24:29 +0200

    added file description, added simple getters for students attributes

--- course.h
+++ course.h
@@ -1,3 +1,13 @@
+/**
+ * @file course.h
+ * @author luka luig (luka@4luig.de)
+ * @brief enum types Degree and Course
+ * @date 2022-10-22
+ *
+ * Copyright (c) 2022 luka luig
+ *
+ */
+
 enum Degree {
 	Bachelor,
 	Master
--- student.h
+++ student.h
@@ -1,3 +1,13 @@
+/**
+ * @file student.h
+ * @author luka luig (luka@4luig.de)
+ * @brief student class
+ * @date 2022-10-22
+ *
+ * Copyright (c) 2022 luka luig
+ *
+ */
+
 #include "course.h"
 
 typedef struct {
@@ -8,3 +18,12 @@ typedef struct {
 	Course course;
 	int semester;
 } Student;
+
+// simple getters
+
+int get_id(Student *student) { return student->student_id; }
+char *get_name(Student *student) { return student->name; }
+char *get_surname(Student *student) { return student->surname; }
+Degree get_degree(Student *student) { return student->degree; }
+Course get_course(Student *student) { return student->course; }
+int get_semester(Student *student) { return student->semester; }
