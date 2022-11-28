6455e8d944d12378f926308a298ebf94b80812db
akulgiul <luka@4luig.de>
2022-11-28 02:53:03 +0100

    simple getters

--- student.c
+++ student.c
@@ -21,19 +21,58 @@ struct Student {
 	int semester;
 };
 
+// constructor
 Student *student_init() {
 	Student *student = malloc(sizeof(Student));
 	return student;
 }
 
+// deconstructor
 void student_destroy(Student *student) {
 	free(student);
 }
 
-void student_set_id(Student *student, int ID){
+// simple setters
+void student_set_id(Student *student, int ID) {
 	student->student_id = ID;
 }
 
-int student_get_id(Student *student){
+void student_set_name(Student *student, char n_name[]) {
+}
+
+void student_set_surname(Student *student, char n_name[]) {
+}
+
+void student_set_degree(Student *student, Degree n_degree) {
+}
+
+void student_set_course(Student *student, Course n_course) {
+}
+
+void student_set_semester(Student *student, int n_semester) {
+}
+
+// simple getters
+int student_get_id(Student *student) {
 	return student->student_id;
+}
+
+char *student_get_name(Student *student) {
+	return student->name;
+}
+
+char *student_get_surname(Student *student) {
+	return student->surname;
+}
+
+Degree student_get_degree(Student *student) {
+	return student->degree;
+}
+
+Course student_get_course(Student *student) {
+	return student->course;
+}
+
+int student_get_semester(Student *student) {
+	return student->semester;
 }
--- student.h
+++ student.h
@@ -22,10 +22,20 @@ Student *student_init();
 // deconstructor
 void student_destroy(Student *student);
 
-//simple setters
+// simple setters
 void student_set_id(Student *student, int ID);
+void student_set_name(Student *student, char n_name[]);
+void student_set_surname(Student *student, char n_name[]);
+void student_set_degree(Student *student, Degree n_degree);
+void student_set_course(Student *student, Course n_course);
+void student_set_semesert(Student *student, int semester);
 
-//simple getters
-int student_get_id(Student *student); 
+// simple getters
+int student_get_id(Student *student);
+char *student_get_name(Student *student);
+char *student_get_surname(Student *student);
+Degree student_get_degree(Student *student);
+Course student_get_course(Student *student);
+int student_get_semester(Student *student);
 
 #endif
